81cde78b5b1f460a173c9b6a7c22a0f9ba431ac0
djmgit <djmdeveloper060796@gmail.com>
2018-04-06 02:34:30 +0530

    Added models for states and tourist spots

--- webapp/main.py
+++ webapp/main.py
@@ -10,7 +10,7 @@ import json
 import re
 import os
 
-pp = Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 if os.environ.get('DATABASE_URL') is None:
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///versions.sqlite3'
@@ -21,5 +21,44 @@ app.config['SECRET_KEY'] = "THIS IS SECRET"
 
 db = SQLAlchemy(app)
 
-class States(db.Model):
-	pass
+class StateDB(db.Model):
+    __tablename__ = 'statedb'
+
+    id = db.Column('software_id', db.Integer, primary_key=True)
+    state_name = db.Column(db.String)
+    places_to_visit = db.Column(db.String)
+
+    def __init__(self, state_name='', places_to_visit=''):
+        self.state_name = state_name
+        self.places_to_visit = places_to_visit
+
+class Spots(db.Model):
+    __tablename__ = 'spots'
+
+    id = db.Column('spot_id', db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    info = db.Column(db.String)
+    special_attraction = db.Column(db.String)
+    things_to_do = db.Column(db.String)
+    time_to_visit = db.Column(db.String)
+    near_by_places = db.Column(db.String)
+    similar_places = db.Column(db.String)
+    how_to_reach = db.Column(db.String)
+
+    def __init__(self, name='', location='', info='', special_attraction='', things_to_do='', time_to_visit='', near_by_places='', similar_places='', how_to_reach=''):
+        self.name = name
+        self.location = location
+        self.info = info
+        self.special_attraction = special_attraction
+        self.things_to_do = things_to_do
+        self.time_to_visit = time_to_visit
+        self.near_by_places = near_by_places
+        self.similar_places = similar_places
+        self.how_to_reach = how_to_reach
+
+db.create_all();
+
+@app.route('/')
+def index():
+	return 'alexa back end!!'

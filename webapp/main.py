d34ee45875928f707a57a4b7362f3b815b357054
djmgit <djmdeveloper060796@gmail.com>
2018-04-10 11:15:58 +0530

    Added users table

--- webapp/main.py
+++ webapp/main.py
@@ -61,6 +61,26 @@ class Spots(db.Model):
         self.similar_places = similar_places
         self.how_to_reach = how_to_reach
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    email = db.Column(db.String(80), primary_key=True, unique=True)
+    password = db.Column(db.String(80))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+    def __repr__(self):
+        return '<User %r>' % self.email
+    def is_authenticated(self):
+        return True
+    def is_active(self):
+        return True
+    def is_anonymous(self):
+        return False
+    def get_id(self):
+        return str(self.email)
+
 db.create_all();
 
 class StateDBView(ModelView):
